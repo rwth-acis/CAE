41722a78e1243220ea424f09381946beaaf73d77
Mari0 <mario.rosenstengel@rwth-aachen.de>
2017-10-08 19:24:40 +0200

    added myCAE http server script to supervisor to get acess to generated code

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo ln -sf $HOME/acis/CAE/conf/role.conf /etc/supervisor/conf.d/role.conf
 sudo ln -sf $HOME/acis/CAE/conf/services.conf /etc/supervisor/conf.d/services.conf
 sudo ln -sf $HOME/acis/CAE/conf/services_debug.conf /etc/supervisor/conf.d/services_debug.conf
 sudo ln -sf $HOME/acis/CAE/conf/widgets.conf /etc/supervisor/conf.d/widgets.conf
+sudo ln -sf $HOME/acis/CAE/conf/myCAE.conf /etc/supervisor/conf.d/myCAE.conf
 
 sudo ln -sf $HOME/acis/CAE/conf/syncmeta_dev.conf /etc/supervisor/conf.d/syncmeta_dev.conf
 sudo ln -sf $HOME/acis/CAE/conf/wireframing_dev.conf /etc/supervisor/conf.d/wireframing_dev.conf
