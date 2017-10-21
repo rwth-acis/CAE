7cfb6d8cf97922ec7f1a59cbe755e7f367863946
Mari0 <mario.rosenstengel@rwth-aachen.de>
2017-10-21 15:54:54 +0200

    a minor improvement for the frontend component persistence widget

--- cloud10_deploy.sh
+++ cloud10_deploy.sh
@@ -7,13 +7,13 @@ echo "DEPLOY LAS2PEER SERVICES"
 echo "=> Copy files of the model persistence service"
 persistence=CAE-Model-Persistence-Service
 
-scp $persistence/service/*.jar cae@cloud10.dbis.rwth-aachen.de:./services/CAE-Server/service
-scp $persistence/lib/*.jar  cae@cloud10.dbis.rwth-aachen.de:./services/CAE-Server/lib
+scp $persistence/service/*.jar cae@cloud10.dbis.rwth-aachen.de:./services2/CAE-Server/service
+scp $persistence/lib/*.jar  cae@cloud10.dbis.rwth-aachen.de:./services2/CAE-Server/lib
 echo "		DONE"
 
 echo "=> Copy file of the code generation service"
 codegeneration=CAE-Code-Generation-Service
 
-scp $codegeneration/service/*.jar cae@cloud10.dbis.rwth-aachen.de:./services/CAE-Server/service
-scp $codegeneration/lib/*.jar cae@cloud10.dbis.rwth-aachen.de:./services/CAE-Server/lib
+scp $codegeneration/service/*.jar cae@cloud10.dbis.rwth-aachen.de:./services2/CAE-Server/service
+scp $codegeneration/lib/*.jar cae@cloud10.dbis.rwth-aachen.de:./services2/CAE-Server/lib
 echo "		DONE"
